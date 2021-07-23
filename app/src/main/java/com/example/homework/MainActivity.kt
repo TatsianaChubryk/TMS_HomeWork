20423d8591ef72b39648b63d76d2fea91e65f5b5
TatsianaChubryk <tatsianachubryk@gmail.com>
2021-07-23 22:03:53 +0300

    Add task1, task2 Homework 16

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.homework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnTaskOne:Button = findViewById(R.id.btn_taskOne)
+        btnTaskOne.setOnClickListener {
+            val intent = Intent(this, TaskOneActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnTaskTwo:Button = findViewById(R.id.btn_taskTwo)
+        btnTaskTwo.setOnClickListener {
+            val intent = Intent(this, TaskTwoActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnTaskThree:Button = findViewById(R.id.btn_taskThree)
+        btnTaskThree.setOnClickListener {
+            val intent = Intent(this, TaskThreeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/homework/TaskThreeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*Написать программу определения оценки студента по его рейтингу, на основе следующих правил:
+рейтинг Оценка
+0-19 F
+20-39 E
+40-59 D
+60-74 C
+75-89 B
+90-100 A*/
+
+
+class TaskThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_three)
+    }
+}
