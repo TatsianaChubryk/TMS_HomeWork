bb2f23fe208839050de976fa7131d2226cfe3a1c
TatsianaChubryk <tatsianachubryk@gmail.com>
2021-07-29 20:55:23 +0300

    Homework 17 (timer)

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.homework
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+/*обратный отсчет (таймер)*/
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etSecond: EditText
+    private lateinit var btnStart: Button
+    private lateinit var tvTimer: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+
+        btnStart.setOnClickListener{
+            timer(second = etSecond.text.toString().toLong())
+        }
+    }
+
+    private fun timer(second: Long){
+        object : CountDownTimer(second * 1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                tvTimer.text = (millisUntilFinished / 1000).toString()
+            }
+            override fun onFinish() {
+                tvTimer.text = getString(R.string.done)
+            }
+        }.start()
+    }
+
+    private fun initView(){
+        etSecond = findViewById(R.id.etSecond)
+        btnStart = findViewById(R.id.btnStart)
+        tvTimer = findViewById(R.id.tvTimer)
     }
 }
