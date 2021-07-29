eca188e19ef1ce56d7ee5e42b4a23ca77226f9e7
TatsianaChubryk <tatsianachubryk@gmail.com>
2021-07-29 22:55:31 +0300

    Homework 18 (activity_main)

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.homework
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
