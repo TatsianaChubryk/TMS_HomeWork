a1f14c6c28a58394882dac3909d2dccbaf30fe45
TatsianaChubryk <84435613+TatsianaChubryk@users.noreply.github.com>
2021-08-14 11:07:21 +0300

    Homework 21 (mediaplayer)

--- app/src/main/java/com/example/homework/hw/homework/lesson21/MediaPlayerFragment.kt
+++ app/src/main/java/com/example/homework/hw/homework/lesson21/MediaPlayerFragment.kt
@@ -2,13 +2,16 @@ package com.example.homework.hw.homework.lesson21
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import com.example.homework.R
 import com.example.homework.databinding.MediaPlayerFragmentBinding
+import java.lang.Exception
 
 class MediaPlayerFragment : Fragment(), View.OnClickListener {
 
@@ -18,39 +21,83 @@ class MediaPlayerFragment : Fragment(), View.OnClickListener {
     private var currentSong: MutableList<Int> = mutableListOf(R.raw.music)
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         _binding = MediaPlayerFragmentBinding.inflate(inflater, container, false)
         return binding?.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.btnPlay?.setOnClickListener(this)
         soundControl(currentSong[0])
     }
 
-    private fun soundControl(id: Int){
+    private fun soundControl(id: Int) {
         binding?.btnPlay?.setOnClickListener {
             if (mp == null) {
-                mp = MediaPlayer.create(this, id)
-                Log.d("MainActivity", "ID: ${mp!!.audioSessionId}")
+                mp = MediaPlayer.create(context, id)
 
+                initSeekBar()
             }
             mp?.start()
-            Log.d("MainActivity", "Duration: ${mp!!.audioSessionId}")
         }
+
+        binding?.btnPause?.setOnClickListener {
+            if (mp != null) {
+                mp?.pause()
+            }
+        }
+
+        binding?.btnStop?.setOnClickListener {
+            if (mp != null) {
+                mp?.stop()
+                mp?.reset()
+                mp?.release()
+                mp = null
+            }
+        }
+
+        binding?.seekbar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) mp?.seekTo(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+    }
+
+    private fun initSeekBar() {
+        binding?.seekbar?.max = mp!!.duration
+
+        val handler = Handler()
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                try {
+                    binding?.seekbar?.progress = mp!!.currentPosition
+                    handler.postDelayed(this, 1000)
+                } catch (e: Exception) {
+                    binding?.seekbar?.progress = 0
+                }
+            }
+        }, 0)
     }
 
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-
     }
 }
